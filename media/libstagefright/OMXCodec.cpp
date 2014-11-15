866abfe84f4c9818fc869b3d545c4fdfb7c04307
Praveen Chavan <pchavan@codeaurora.org>
2014-11-14 16:52:02 -0800

    Stagefright: ExtendedStats: simplify bitrate calculation
    Replace usage of stl::SortedVector with a static array to
    calculate moving average
    
    Change-Id: Ic8f9d8320c30d01090d7a53ddee9d1b168f76a42

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1679,6 +1679,11 @@ status_t OMXCodec::setVideoOutputFormat(
     }
 
     ////////////////////////////////////////////////////////////////////////////
+    int32_t frameRate;
+    if (meta->findInt32(kKeyFrameRate, &frameRate)) {
+            PLAYER_STATS(setFrameRate, frameRate);
+    }
+    ////////////////////////////////////////////////////////////////////////////
 
     InitOMXParams(&def);
     def.nPortIndex = kPortIndexOutput;
