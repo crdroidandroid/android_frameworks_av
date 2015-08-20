c8411c614df3cb9246a448df9e4c2a03399ebb7b
Mingming Yin <mingming@codeaurora.org>
2015-08-19 20:32:33 -0700

    audio: add encoding support for linear PCM format
    - add encoding support for linear PCM format.
    
    [neobuddy89: Squashed commits.]
    
    Change-Id: I3f29e6c23a511a29c81e7083673f9138cb2e75f7

--- media/libmediaplayerservice/StagefrightRecorder.h
+++ media/libmediaplayerservice/StagefrightRecorder.h
@@ -258,7 +258,7 @@ private:
     void clipNumberOfAudioChannels();
     void setDefaultProfileIfNecessary();
     void setDefaultVideoEncoderIfNecessary();
-
+    virtual sp<MediaSource> setPCMRecording() {return NULL;}
 
     StagefrightRecorder(const StagefrightRecorder &);
     StagefrightRecorder &operator=(const StagefrightRecorder &);
