7eef7eafba993da5014f7d07b9dff9e080c2f6f8
Mingming Yin <mingming@codeaurora.org>
2015-08-19 20:32:33 -0700

    audio: add encoding support for linear PCM format
    - add encoding support for linear PCM format.
    
    [neobuddy89: Squashed commits.]
    
    Change-Id: I3f29e6c23a511a29c81e7083673f9138cb2e75f7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1445,14 +1445,14 @@ sp<MediaCodecSource> StagefrightRecorder::createAudioSource() {
 
     sp<MediaCodecSource> audioEncoder =
             MediaCodecSource::Create(mLooper, format, audioSource);
-    sp<AudioSystem::AudioDeviceCallback> callback = mAudioDeviceCallback.promote();
-    if (mDeviceCallbackEnabled && callback != 0) {
-        audioSource->addAudioDeviceCallback(callback);
-    }
-    mAudioSourceNode = audioSource;
-
     if (audioEncoder == NULL) {
         ALOGE("Failed to create audio encoder");
+    } else {
+        sp<AudioSystem::AudioDeviceCallback> callback = mAudioDeviceCallback.promote();
+        if (mDeviceCallbackEnabled && callback != 0) {
+            audioSource->addAudioDeviceCallback(callback);
+        }
+        mAudioSourceNode = audioSource;
     }
 
     return audioEncoder;
@@ -1514,13 +1514,22 @@ status_t StagefrightRecorder::setupRawAudioRecording() {
     }
 
     sp<MediaCodecSource> audioEncoder = createAudioSource();
-    if (audioEncoder == NULL) {
+    if (audioEncoder != NULL) {
+        CHECK(mWriter != 0);
+        mWriter->addSource(audioEncoder);
+        mAudioEncoderSource = audioEncoder;
+    } else if (audioEncoder == NULL && mAudioEncoder == AUDIO_ENCODER_LPCM) {
+        CHECK(mWriter != 0);
+        sp<MediaSource> src = setPCMRecording();
+        if (src == NULL) {
+            ALOGE("Recording source is null");
+            return UNKNOWN_ERROR;
+        }
+        mAudioSourceNode =  reinterpret_cast<AudioSource* > (src.get());
+        mWriter->addSource(src);
+    } else if (audioEncoder == NULL) {
         return UNKNOWN_ERROR;
     }
-
-    CHECK(mWriter != 0);
-    mWriter->addSource(audioEncoder);
-    mAudioEncoderSource = audioEncoder;
 
     if (mMaxFileDurationUs != 0) {
         mWriter->setMaxFileDuration(mMaxFileDurationUs);
