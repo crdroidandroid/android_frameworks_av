a69e5c1dc55b24825a1b531993b3ac93619c50d5
Simon Shields <keepcalm444@gmail.com>
2016-10-06 20:25:32 +1100

    camera/media: Support legacy HALv1 camera in mediaserver
    Pre 7.0 camera HALv1 can not share its video buffers
    across different processes, which requires us to
    disable this security feature.
    
    This change allows devices to re-integrate cameraserver
    and mediaserver which is the first step to support older
    prebuilt camera HALs. A follow-up change will add back
    support for legacy buffer handling.
    
    To enable:
    TARGET_HAS_LEGACY_CAMERA_HAL1 := true
    media.stagefright.legacyencoder=true
    media.stagefright.less-secure=true
    
    Change-Id: I4fcc8907ea235b7e83af26122b4da97ca5117816
    
    mediaserver: Update HALv1 dependencies for 8.1.0
    
     * Add libcameraservice shared library dependency
     * Add new libgui, android.hardware.camera.common@1.0
        and android.hardware.camera.provider@2.4 shared
        libraries and exported headers HAL dependencies
    
     * Match cameraserver HIDL additions and dependencies
    
     * When TARGET_HAS_LEGACY_CAMERA_HAL1 is set, the mediaserver
        includes CameraService.h but exported headers are missing
    
     * Error upon build due to missing libcameraservice linkage:
        fatal error: 'android/hardware/BnCameraService.h' file not found
    
    mediaserver: Update HALv1 dependencies for 9.0.0
    
     * Disable active state UID validation for HAL1 devices
    
    Change-Id: Ib6bb8a4e9ef18606c64e2dff13504d2eeaac13b1
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1011,7 +1011,11 @@ int32_t CameraService::mapToInterface(StatusInternal status) {
 Status CameraService::initializeShimMetadata(int cameraId) {
     int uid = CameraThreadState::getCallingUid();
 
+#ifdef NO_CAMERA_SERVER
+    String16 internalPackageName("media");
+#else
     String16 internalPackageName("cameraserver");
+#endif
     String8 id = String8::format("%d", cameraId);
     Status ret = Status::ok();
     sp<Client> tmp = nullptr;
@@ -1092,7 +1096,9 @@ Status CameraService::getLegacyParametersLazy(int cameraId,
 static bool isTrustedCallingUid(uid_t uid) {
     switch (uid) {
         case AID_MEDIA:        // mediaserver
+#ifndef NO_CAMERA_SERVER
         case AID_CAMERASERVER: // cameraserver
+#endif
         case AID_RADIO:        // telephony
             return true;
         default:
@@ -1225,6 +1231,7 @@ Status CameraService::validateClientPermissionsLocked(const String8& cameraId,
                 clientName8.string(), clientUid, clientPid, cameraId.string());
     }
 
+#ifndef NO_CAMERA_SERVER
     // Make sure the UID is in an active state to use the camera
     if (!mUidPolicy->isUidActive(callingUid, String16(clientName8))) {
         int32_t procState = mUidPolicy->getProcState(callingUid);
@@ -1236,6 +1243,7 @@ Status CameraService::validateClientPermissionsLocked(const String8& cameraId,
                 clientName8.string(), clientUid, clientPid, cameraId.string(),
                 callingUid, procState);
     }
+#endif
 
     // If sensor privacy is enabled then prevent access to the camera
     if (mSensorPrivacyPolicy->isSensorPrivacyEnabled()) {
