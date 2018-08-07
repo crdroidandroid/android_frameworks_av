55dd31091332f98c7b24f79aab5324b54ed1d701
darkobas <darkobas@gmail.com>
2018-08-07 13:00:17 +0200

    libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO
    write camera package name if matches
    
    Change-Id: Ia84d5f7f3961945e2143968eb572f5d9106d0297
    
    libcameraservice: write all pkgNames
    
    Change-Id: I85c2e9d4ffb56cc08aea1c1eabc1ed2ec45c0217
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #include <ctime>
 #include <string>
+#ifdef TARGET_NEEDS_CLIENT_INFO
+#include <iostream>
+#include <fstream>
+#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -2537,6 +2541,11 @@ status_t CameraService::BasicClient::startCameraOps() {
 
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
+#ifdef TARGET_NEEDS_CLIENT_INFO
+    std::ofstream cpf("/data/vendor/lineage/client_package_name");
+    std::string cpn = String8(mClientPackageName).string();
+    cpf << cpn;
+#endif
     return OK;
 }
 
