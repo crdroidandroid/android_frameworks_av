ea79bba8749126bda9922c3caee443256c354b87
darkobas <darkobas@gmail.com>
2018-08-07 13:00:17 +0200

    libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO
    write camera package name if matches
    
    Change-Id: Ia84d5f7f3961945e2143968eb572f5d9106d0297
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #include <ctime>
 #include <string>
+#ifdef TARGET_NEEDS_CLIENT_INFO
+#include <iostream>
+#include <fstream>
+#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -1225,7 +1229,6 @@ Status CameraService::connect(
                 ret.toString8());
         return ret;
     }
-
     *device = client;
     return ret;
 }
@@ -2279,6 +2282,15 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->updateProxyDeviceState(ICameraServiceProxy::CAMERA_STATE_OPEN,
             mCameraIdStr, mCameraFacing, mClientPackageName, apiLevel);
 
+#ifdef TARGET_NEEDS_CLIENT_INFO
+    std::ofstream cpf("/data/misc/camera/client_package_name");
+    std::string cpn = String8(mClientPackageName).string();
+    if (cpn.compare("com.oneplus.camera") == 0) {
+        cpf << "com.oneplus.camera";
+    } else {
+        cpf << "0";
+    }
+#endif
     return OK;
 }
 
