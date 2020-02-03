1e097aade0c9b59bb3fdd4e436b6be8b96820d98
Jake Weinstein <jake@aospa.co>
2020-02-03 13:33:55 +0000

    Revert "libstagefright: Free buffers on observer died"
    This reverts commit 50d068143e9d417c19df34903e071ca629097606.
    
    Reason for revert: We already had this, see above. Thanks to Wang Han for pointing this out.
    
    Change-Id: Ic31e422c4374058d6234bb8c68d39cc9dc8f4716

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -514,9 +514,6 @@ status_t OMXNodeInstance::freeNode() {
 
     Mutex::Autolock _l(mLock);
 
-    if (mActiveBuffers.size() > 0) {
-        freeActiveBuffers();
-    }
     status_t err = mOwner->freeNode(this);
 
     mDispatcher.clear();
