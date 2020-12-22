251df6acfba8945d65ca85f44ac5909cdea4b8e4
V S Ganga VaraPrasad (VARA) Adabala <vadabala@codeaurora.org>
2020-12-22 14:35:43 +0530

    Updating code diff from last commit
    Change-Id: I2ada754b20bebecc816b81c73c996eb9cd50a15e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/api2/HeicCompositeStream.cpp
+++ services/camera/libcameraservice/api2/HeicCompositeStream.cpp
@@ -1378,7 +1378,9 @@ status_t HeicCompositeStream::initializeCodec(uint32_t width, uint32_t height,
     mOutputWidth = width;
     mOutputHeight = height;
     mAppSegmentMaxSize = calcAppSegmentMaxSize(cameraDevice->info());
-    mMaxHeicBufferSize = (ALIGN(mOutputWidth, 512) * ALIGN(mOutputHeight, 512)) * 3 / 2 + mAppSegmentMaxSize;
+    mMaxHeicBufferSize =
+        ALIGN(mOutputWidth, HeicEncoderInfoManager::kGridWidth) *
+        ALIGN(mOutputHeight, HeicEncoderInfoManager::kGridHeight) * 3 / 2 + mAppSegmentMaxSize;
 
     return OK;
 }
