19f7288d9aaef5c1dd8a1a5d975b0eaf4078e34a
Wonsik Kim <wonsik@google.com>
2021-04-01 12:45:18 -0700

    stagefright: add nullptr check in MediaCodecSource
    Bug: 183068143
    Test: bug repro steps
    Change-Id: I53cc2cba1077091faa48b3cd438abd8b160039e8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -680,6 +680,9 @@ void MediaCodecSource::resume(int64_t resumeStartTimeUs) {
 status_t MediaCodecSource::feedEncoderInputBuffers() {
     MediaBufferBase* mbuf = NULL;
     while (!mAvailEncoderInputIndices.empty() && mPuller->readBuffer(&mbuf)) {
+        if (!mEncoder) {
+            return BAD_VALUE;
+        }
         size_t bufferIndex = *mAvailEncoderInputIndices.begin();
         mAvailEncoderInputIndices.erase(mAvailEncoderInputIndices.begin());
 
@@ -1091,7 +1094,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
         if (mFlags & FLAG_USE_SURFACE_INPUT) {
             sp<AMessage> params = new AMessage;
             params->setInt64(PARAMETER_KEY_OFFSET_TIME, mInputBufferTimeOffsetUs);
-            err = mEncoder->setParameters(params);
+            err = mEncoder ? mEncoder->setParameters(params) : BAD_VALUE;
         }
 
         sp<AMessage> response = new AMessage;
@@ -1111,7 +1114,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
         if (mFlags & FLAG_USE_SURFACE_INPUT) {
             sp<AMessage> params = new AMessage;
             params->setInt64("stop-time-us", stopTimeUs);
-            err = mEncoder->setParameters(params);
+            err = mEncoder ? mEncoder->setParameters(params) : BAD_VALUE;
         } else {
             err = mPuller->setStopTimeUs(stopTimeUs);
         }
