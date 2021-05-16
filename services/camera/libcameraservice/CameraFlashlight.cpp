c38bfdb6060d49b9349e5c3373ba3dd5aa4efd3c
kerneltoast <sultan@kerneltoast.com>
2021-05-16 16:36:51 +0530

    Fix high battery drain after using flashlight After turning off the flashlight via the tile, the camera is kept open, preventing the device from entering deep sleep (and destroying battery life).
    Properly close the camera after turning off the flashlight in order to fix the high battery drain.
    
    Change-Id: Ib3b1fa77f8f1d22b94ab969dff834015a7ebe59b
    Signed-off-by: adiatul16 <adiatul16@gmail.com>

--- services/camera/libcameraservice/CameraFlashlight.cpp
+++ services/camera/libcameraservice/CameraFlashlight.cpp
@@ -587,7 +587,7 @@ status_t CameraHardwareInterfaceFlashControl::disconnectCameraDevice() {
     }
     mDevice->setPreviewWindow(NULL);
     mDevice->release();
-    mDevice = NULL;
+    mDevice.clear();
 
     return OK;
 }
