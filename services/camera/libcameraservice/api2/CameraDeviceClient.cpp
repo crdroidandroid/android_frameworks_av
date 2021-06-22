76f9b0f5a4073871e014d0086f885072089fe423
Pranav Vashi <neobuddy89@gmail.com>
2021-06-22 07:51:19 +0530

    fixup! CameraDeviceClient: fix recognition of Y16 format
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/api2/CameraDeviceClient.cpp
+++ services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -1568,7 +1568,7 @@ bool CameraDeviceClient::roundBufferDimensionNearest(int32_t width, int32_t heig
         }
     }
 
-    if (format == HAL_PIXEL_FORMAT_Y16) {
+    if (bestWidth == -1 && format == HAL_PIXEL_FORMAT_Y16) {
         bestWidth = width;
         bestHeight = height;
     }
