d153907a31ed6ef5ebd1b296de831774245be7cc
xNombre <kartapolska@gmail.com>
2021-08-17 13:12:46 +0200

    Revert "Fix high battery drain after using flashlight After turning off the flashlight via the tile, the camera is kept open, preventing the device from entering deep sleep (and destroying battery life)."
    This reverts commit 31e163657471e829a1f227ca69dd01b06ebeaaae.
    
    Assigning NULL to sp will eventually execute its destructor.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- services/camera/libcameraservice/CameraFlashlight.cpp
+++ services/camera/libcameraservice/CameraFlashlight.cpp
@@ -587,7 +587,7 @@ status_t CameraHardwareInterfaceFlashControl::disconnectCameraDevice() {
     }
     mDevice->setPreviewWindow(NULL);
     mDevice->release();
-    mDevice.clear();
+    mDevice = NULL;
 
     return OK;
 }
