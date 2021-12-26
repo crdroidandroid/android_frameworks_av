79fea5b5727b6cd92dbe381fee702fb85069a097
Chenyang Zhong <zhongcy95@gmail.com>
2021-12-25 22:06:27 -0500

    WifiDisplaySource: allow overriding max wfd resolution with prop
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I394c85901be2e3daa1a22f52e787e920a78289c5

--- media/libstagefright/wifi-display/source/WifiDisplaySource.cpp
+++ media/libstagefright/wifi-display/source/WifiDisplaySource.cpp
@@ -78,12 +78,14 @@ WifiDisplaySource::WifiDisplaySource(
 
     mSupportedSourceVideoFormats.disableAll();
 
-    mSupportedSourceVideoFormats.setNativeResolution(
-            VideoFormats::RESOLUTION_CEA, 8);  // 1920x1080p60
+    // Set default all resolutions up to 1920x1080p60
+    int32_t maxWfdRes = property_get_int32("media.wfd.max_resolution", 8); // 1920x1080p60
 
-    // Enable all resolutions up to 1920x1080p60
+    mSupportedSourceVideoFormats.setNativeResolution(
+            VideoFormats::RESOLUTION_CEA, maxWfdRes);
+
     mSupportedSourceVideoFormats.enableResolutionUpto(
-            VideoFormats::RESOLUTION_CEA, 8,
+            VideoFormats::RESOLUTION_CEA, maxWfdRes,
             VideoFormats::PROFILE_CHP,  // Constrained High Profile
             VideoFormats::LEVEL_32);    // Level 3.2
 }
