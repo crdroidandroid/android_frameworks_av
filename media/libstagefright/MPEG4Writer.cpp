14ba1a0ee0a9014f5be55ea729971fd9fd1d3108
Malathi Gottam <quic_mgottam@quicinc.com>
2022-02-09 23:14:59 +0530

    stagefright: error handling during file size limit set
    During exhaustive concurrency usecases execution, sometimes
    return from filepath configurable limit may be negative in case
    any error is encountered. Determining file size limit based on
    such return value can result in undefined behaviour. So added
    check to error out in such scenarios.
    
    CRs-Fixed: 3127997
    Change-Id: I7fdebff0fb57b72e4cb7f39b8a96b18ded0a7f3b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/libstagefright/MPEG4Writer.cpp
+++ media/libstagefright/MPEG4Writer.cpp
@@ -865,6 +865,12 @@ status_t MPEG4Writer::start(MetaData *param) {
      */
     int32_t fileSizeBits = fpathconf(mFd, _PC_FILESIZEBITS);
     ALOGD("fpathconf _PC_FILESIZEBITS:%" PRId32, fileSizeBits);
+
+    if (fileSizeBits < 0) {
+        ALOGE("fpathconf(%d) failed: %d, %s", mFd, fileSizeBits, strerror(errno));
+        return UNKNOWN_ERROR;
+    }
+
     fileSizeBits = std::min(fileSizeBits, 52 /* cap it below 4 peta bytes */);
     int64_t maxFileSizeBytes = ((int64_t)1 << fileSizeBits) - 1;
     if (mMaxFileSizeLimitBytes > maxFileSizeBytes) {
