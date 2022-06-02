50b3274854e990add1eaadefe9ddc093dafc3bbd
Pranav Vashi <neobuddy89@gmail.com>
2022-06-02 18:23:53 +0530

    av: Initial support for 24bit encoding and 6 channels
    * From CAF.
    
    Change-Id: I32bb7470c2abede313d6392dede4873270d720cc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/libstagefright/DataConverter.cpp
+++ media/libstagefright/DataConverter.cpp
@@ -24,6 +24,9 @@
 #include <media/MediaCodecBuffer.h>
 #include <media/stagefright/foundation/ADebug.h>
 #include <media/stagefright/foundation/AUtils.h>
+#include <system/audio.h>
+#include <audio_utils/primitives.h>
+#include <audio_utils/format.h>
 
 namespace android {
 
@@ -81,12 +84,34 @@ size_t SampleConverterBase::targetSize(size_t sourceSize) {
     return numSamples * mTargetSampleSize;
 }
 
+static audio_format_t getAudioFormat(AudioEncoding e) {
+    audio_format_t format = AUDIO_FORMAT_INVALID;
+    switch (e) {
+        case kAudioEncodingPcm16bit:
+            format = AUDIO_FORMAT_PCM_16_BIT;
+            break;
+        case kAudioEncodingPcm8bit:
+            format = AUDIO_FORMAT_PCM_8_BIT;
+            break;
+        case kAudioEncodingPcmFloat:
+            format = AUDIO_FORMAT_PCM_FLOAT;
+            break;
+       case kAudioEncodingPcm24bitPacked:
+            format = AUDIO_FORMAT_PCM_24_BIT_PACKED;
+            break;
+        default:
+            ALOGE("Invalid AudioEncoding %d", e);
+        }
+        return format;
+}
 
 static size_t getAudioSampleSize(AudioEncoding e) {
     switch (e) {
-        case kAudioEncodingPcm16bit: return 2;
-        case kAudioEncodingPcm8bit:  return 1;
-        case kAudioEncodingPcmFloat: return 4;
+        case kAudioEncodingPcm16bit:
+        case kAudioEncodingPcm8bit:
+        case kAudioEncodingPcmFloat:
+        case kAudioEncodingPcm24bitPacked:
+            return audio_bytes_per_sample(getAudioFormat(e));
         default: return 0;
     }
 }
