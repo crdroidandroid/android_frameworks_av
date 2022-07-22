99d97c26604c5c39d380b7063682f09a37a79154
cjh1249131356 <cjh1249131356@gmail.com>
2022-07-22 22:55:18 +0800

    libcameraservice: Add support to set vendor tag with client package name
    * OEMs like OnePlus and Nothing detect camera package name to unlock features like 48mp.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -142,6 +142,9 @@ const String16 CameraService::kWatchAllClientsFlag("all");
 // Set to keep track of logged service error events.
 static std::set<String8> sServiceErrorEventSet;
 
+// Current camera package name
+static std::string sCurrPackageName;
+
 CameraService::CameraService() :
         mEventLog(DEFAULT_EVENT_LOG_LENGTH),
         mNumberOfCameras(0),
@@ -935,6 +938,10 @@ Status CameraService::filterGetInfoErrorCode(status_t err) {
                     "Camera HAL encountered error %d: %s",
                     err, strerror(-err));
     }
+}
+
+std::string CameraService::getCurrPackageName() {
+    return sCurrPackageName;
 }
 
 Status CameraService::makeClient(const sp<CameraService>& cameraService,
@@ -1815,6 +1822,8 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
     ALOGI("CameraService::connect call (PID %d \"%s\", camera ID %s) and "
             "Camera API version %d", packagePid, clientName8.string(), cameraId.string(),
             static_cast<int>(effectiveApiLevel));
+
+    sCurrPackageName = clientName8.string();
 
     nsecs_t openTimeNs = systemTime();
 
