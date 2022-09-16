8dfa64e2cf0f5450da9856289dfc021379300d58
Yanqiang Fan <yanqiang.fan@mediatek.com>
2022-09-16 16:06:55 +0800

    fix access null pointer exception
    If IComponentStore/default service is died,
    the transResult of hwbinder call through hidl will return not Ok,
    then the "base" parameter transfered to Codec2ConfigurableClient
    will be null, it will implement base->getName() without null pointer check.
    which will cause exception.
    
    Test: MTBF
    Change-Id: Ibecd5343eda8f80ad847339148a8e57588f0fb11

--- media/codec2/hidl/client/client.cpp
+++ media/codec2/hidl/client/client.cpp
@@ -266,12 +266,18 @@ Codec2ConfigurableClient::Codec2ConfigurableClient(
       : mBase{base},
         mName{[base]() -> C2String {
                 C2String outName;
+                if(base == nullptr) {
+                    return "";
+                }
                 Return<void> transStatus = base->getName(
                         [&outName](const hidl_string& name) {
                             outName = name.c_str();
                         });
                 return transStatus.isOk() ? outName : "";
             }()} {
+    if(!mBase) {
+        LOG(ERROR) << "Codec2ConfigurableClient -- transaction failed.";
+    }
 }
 
 c2_status_t Codec2ConfigurableClient::query(
