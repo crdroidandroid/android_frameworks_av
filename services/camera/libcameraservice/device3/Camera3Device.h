24e12c7ce89458237336d08ff623c06c72e6e694
Kwangkyu Park <kk48.park@samsung.com>
2023-02-09 16:29:10 +0900

    Fix an issue that a free memory is accessed
    In error state, request thread join is not executed. And,
    Camera3Device's mId can be accessed from String& after being freed with
    CLOGE().
    
    This fix will address by changing reference type to normal variable.
    
    Bug: 268437033
    Google: 2428574
    Change-Id: Iad3fcd0fbbaf1e18a2b95093aba08015a933374c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/device3/Camera3Device.h
+++ services/camera/libcameraservice/device3/Camera3Device.h
@@ -1013,7 +1013,7 @@ class Camera3Device :
 
         wp<NotificationListener> mListener;
 
-        const String8&     mId;       // The camera ID
+        const String8      mId;       // The camera ID
         int                mStatusId; // The RequestThread's component ID for
                                       // status tracking
 
