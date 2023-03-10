5d17cdfa90257f9a771794f809d7a9dfa2082658
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-03-09 21:25:49 -0800

    Revert "MediaCodec: Don't disconnectFromSurface when it is using"
    This reverts commit 76ec0073140e224a1fd8afd6275b985c77ed2935.
    
    Causes errors with exoplayer2 not being able to reconnect
    to surfaces that it expected to be released.
    
    See https://github.com/crdroidandroid/issue_tracker/issues/35
    
    Abandoned: https://android-review.googlesource.com/c/platform/frameworks/av/+/2200357
    Change-Id: I0edeb894f41eed95c3d3016d2992afd26938f193

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -4175,7 +4175,7 @@ void MediaCodec::onMessageReceived(const sp<AMessage> &msg) {
                                     err = mCodec->setSurface(surface);
                                 }
                             }
-                            if (err == OK && mSurface->getIGraphicBufferProducer() == nullptr) {
+                            if (err == OK) {
                                 (void)disconnectFromSurface();
                                 mSurface = surface;
                             }
