fe90bce69433658d0961907eb200c40afb3a1570
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-26 21:29:52 +0530

    OMX: Remove support for prebuilt ac4 decoder
    Requires proprietary Dolby changes in libstagefright.
    
    This partially reverts commit 01090fa125c2993654e18e024ddf7cef87bca30e.
    
    Change-Id: Ifdf36ad253155e77ba14d0624612bf6be6711ca6

--- media/libstagefright/omx/SoftOMXPlugin.cpp
+++ media/libstagefright/omx/SoftOMXPlugin.cpp
@@ -64,7 +64,6 @@ static const struct {
     { "OMX.dolby.ac3.decoder", "ddpdec", "audio_decoder.ac3" },
     { "OMX.dolby.eac3-joc.decoder", "ddpdec", "audio_decoder.eac3_joc" },
     { "OMX.dolby.eac3.decoder", "ddpdec", "audio_decoder.eac3" },
-    { "OMX.dolby.ac4.decoder", "ac4dec", "audio_decoder.ac4" },
 };
 
 static const size_t kNumComponents =
