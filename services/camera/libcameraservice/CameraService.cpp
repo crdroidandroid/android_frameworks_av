61fa6393fe6006dbccc40c35a4d93c1c0ef01ee7
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-05 18:48:37 +0900

    Extends "Camera: Skip stream size check for whitelisted apps"
    Squashed commits:
        cameraserver: Let multiple cameras opened by stock camera application
    
        Let conflicting camera devices list be empty and let the camera hal manage it.
    
        Old camera hals mark camera device as conflicting if the new camera device is opened.
        Fixes dual video camera mode for old devices
    
        Change-Id: I2043dee762ddcef96432e440cc6017181950a4c3
        Signed-off-by: Lostark13 <tushar9915@gmail.com>
    
        cameraserver: Allow google camera to access multiple cameras
    
        Change-Id: I99a735d7bbe2ab4650f40294cf8b776bbb3a8f03
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Co-authored-by: Lostark13 <tushar9915@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I4348bb5d82721cbd7a922cd32a66502d6dda68c9
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -38,6 +38,8 @@
 #include <aidl/AidlCameraService.h>
 #include <android-base/macros.h>
 #include <android-base/parseint.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 #include <android_companion_virtualdevice_flags.h>
 #include <android/companion/virtualnative/IVirtualDeviceManagerNative.h>
 #include <binder/ActivityManager.h>
@@ -1910,6 +1912,14 @@ status_t CameraService::checkIfDeviceIsUsable(const std::string& cameraId) const
     return NO_ERROR;
 }
 
+bool isPrivilegedClient(const std::string &packageName) {
+    std::vector<std::string> privilegedClientList = android::base::Split(
+            android::base::GetProperty("persist.vendor.camera.privapp.list", ""), ",");
+    auto it = std::find(privilegedClientList.begin(), privilegedClientList.end(),
+            packageName);
+    return it != privilegedClientList.end();
+}
+
 void CameraService::finishConnectLocked(const sp<BasicClient>& client,
         const CameraService::DescriptorPtr& desc, int oomScoreOffset, bool systemNativeClient) {
 
@@ -1918,6 +1928,10 @@ void CameraService::finishConnectLocked(const sp<BasicClient>& client,
             CameraService::CameraClientManager::makeClientDescriptor(client, desc,
                     oomScoreOffset, systemNativeClient);
     auto evicted = mActiveClientManager.addAndEvict(clientDescriptor);
+
+    if (isPrivilegedClient(client->getPackageName())) {
+        evicted.clear();
+    }
 
     logConnected(desc->getKey(), static_cast<int>(desc->getOwnerId()),
             client->getPackageName());
@@ -2065,6 +2079,10 @@ status_t CameraService::handleEvictionsLocked(const std::string& cameraId, int c
 
         // Find clients that would be evicted
         auto evicted = mActiveClientManager.wouldEvict(clientDescriptor);
+
+        if (isPrivilegedClient(packageName)) {
+            evicted.clear();
+        }
 
         // If the incoming client was 'evicted,' higher priority clients have the camera in the
         // background, so we cannot do evictions
