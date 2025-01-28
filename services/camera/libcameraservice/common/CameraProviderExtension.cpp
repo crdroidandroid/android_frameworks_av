fae9ab2150adf230a8a4c8a573aaa89c6aba6340
John Galt <johngaltfirstrun@gmail.com>
2025-01-28 13:01:49 -0500

    CameraProviderExtension: add enabled bool and always set torch
    This has two changes for two reasons:
     - Enabled bool is for device side workarounds (such as switches which must be reset, etc).
    
     - Always setting torch strength in this path fixes normal flash usage
       (camera flash, screen off torch gestures, etc).
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/camera/libcameraservice/common/CameraProviderExtension.cpp
+++ services/camera/libcameraservice/common/CameraProviderExtension.cpp
@@ -34,6 +34,6 @@ __attribute__((weak)) int32_t getTorchStrengthLevelExt() {
     return 0;
 }
 
-__attribute__((weak)) void setTorchStrengthLevelExt(__unused int32_t torchStrength) {
+__attribute__((weak)) void setTorchStrengthLevelExt(__unused int32_t torchStrength, __unused bool enabled) {
     // Nothing
 }
