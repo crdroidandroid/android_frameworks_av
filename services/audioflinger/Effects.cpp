38066a17edd81c6512605bc345e5acacfb31573c
zhouchenchang <zhouchenchang@xiaomi.com>
2025-03-28 17:49:24 +0800

    Audio: fix ~refbase() crash caused by race condition
    [cause]：When ~SuspendedEffectDesc() is called, mSuspendedEffects race condition cause strong refcount of SuspendedEffectDesc obj is 1, which causing a fatal error.
    [example]:
        1. thread1:
    	AudioFlinger::setEffectSuspended
    		|-> ThreadBase::setEffectSuspended_l
    			|-> EffectChain::setEffectSuspended_l
    				|-> ~SuspendedEffectDesc
    					|-> ~RefBase
        2. thread2:
    	EffectChain::EffectCallback::onEffectDisable
    		|-> EffectCallback::checkSuspendOnEffectEnabled
    			|-> EffectChain::checkSuspendOnEffectEnabled_l
    				|-> incStrong
    [solution]：add mutex for mSuspendedEffects
    
    Google: 3563870
    Change-Id: I344747a4f2cd5ea7620ce74f11625926a23f178a
    Signed-off-by: zhouchenchang <zhouchenchang@xiaomi.com>

--- services/audioflinger/Effects.cpp
+++ services/audioflinger/Effects.cpp
@@ -3393,7 +3393,15 @@ void EffectChain::EffectCallback::checkSuspendOnEffectEnabled(const sp<IAfEffect
         return;
     }
     // in EffectChain context, an EffectBase is always from an EffectModule so static cast is safe
+    if (!threadLocked) {
+        t->mutex().lock();
+    }
+
     c->checkSuspendOnEffectEnabled_l(effect->asEffectModule(), enabled);
+
+    if (!threadLocked) {
+        t->mutex().unlock();
+    }
 }
 
 void EffectChain::EffectCallback::onEffectEnable(const sp<IAfEffectBase>& effect) {
