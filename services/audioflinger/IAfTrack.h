2643701b75ad780d84cf2f5ced528915180b2083
chongyin <v-chongyin@xiaomi.com>
2025-04-14 20:55:54 +0800

    AudioPlaybackCapture: solve patchTrack unable to update speed issue
    PatchRecord is in the output thread of audiohal. Every time the track
    is consumed by the mixer and the ReleaseBuffer is executed, the data
    obtainBuffer is written into the PatchBuffer. When a third-party application
    uses AudioTrack::setPlaybackRate to timestretch the audio data, PatchRecord
    and PatchTrack need to update framecount and speed at the same time. And the
    remotesubmix thread where PatchTrack is located needs to perform timestretch
    processing synchronously to solve the problem of Track::interceptBuffer
    dropping frames.
    
    Signed-off-by: v-chongyin@xiaomi.com
    Google: 3586731
    Change-Id: I6b831b5a86ead8d940b9e0f474268c750b01592c

--- services/audioflinger/IAfTrack.h
+++ services/audioflinger/IAfTrack.h
@@ -455,6 +455,7 @@ public:
     // Internal mute, this is currently only used for bit-perfect playback
     virtual bool getInternalMute() const = 0;
     virtual void setInternalMute(bool muted) = 0;
+    virtual void setTeePatchesPlaybackRate(const AudioPlaybackRate &playbackRate) = 0;
 };
 
 // playback track, used by DuplicatingThread
@@ -625,6 +626,8 @@ public:
             float speed = 1.0f,
             float volume = 1.0f,
             bool muted = false);
+
+    virtual void setPlaybackRate(const AudioPlaybackRate &playbackRate) = 0;
 };
 
 class IAfPatchRecord : public virtual IAfRecordTrack, public virtual IAfPatchTrackBase {
