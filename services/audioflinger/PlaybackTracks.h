a9178a09da06bec86b5d9178219c7b9d7ab0d67d
chongyin <v-chongyin@xiaomi.com>
2025-04-14 20:55:54 +0800

    AudioPlaybackCapture: solve patchTrack unable to update speed issue
    PatchRecord is in the output thread of audiohal. Every time the track
    is consumed by the mixer and the ReleaseBuffer is executed, the data
    obtainBuffer is written into the PatchBuffer. When a third-party application
    uses AudioTrack::setPlaybackRate to timestretch the audio data, PatchRecord
    and PatchTrack need to update framecount and speed at the same time. And the
    remotesubmix thread where PatchTrack is located needs to perform timestretch
    processing synchronously to solve the problem of Track::interceptBuffer
    dropping frames.
    
    Signed-off-by: v-chongyin@xiaomi.com
    Google: 3586731
    Change-Id: I6b831b5a86ead8d940b9e0f474268c750b01592c

--- services/audioflinger/PlaybackTracks.h
+++ services/audioflinger/PlaybackTracks.h
@@ -314,6 +314,11 @@ protected:
     int8_t& retryCount() final { return mRetryCount; }
     FastTrackUnderruns& fastTrackUnderruns() final { return mObservedUnderruns; }
 
+    void setTeePatchesPlaybackRate (const AudioPlaybackRate &playbackRate) override {
+        forEachTeePatchTrack_l([playbackRate](const auto& patchTrack) {
+            patchTrack->setPlaybackRate(playbackRate);
+        });
+    }
 protected:
     mutable FillingStatus mFillingStatus;
     int8_t              mRetryCount;
@@ -522,6 +527,8 @@ public:
     // PatchProxyBufferProvider interface
     status_t obtainBuffer(Proxy::Buffer* buffer, const struct timespec* timeOut = nullptr) final;
     void releaseBuffer(Proxy::Buffer* buffer) final;
+
+    void setPlaybackRate (const AudioPlaybackRate &playbackRate) override;
 
 private:
     void restartIfDisabled() override;
--- services/audioflinger/Tracks.cpp
+++ services/audioflinger/Tracks.cpp
@@ -2797,6 +2797,12 @@ void PatchTrack::releaseBuffer(Proxy::Buffer* buffer)
     }
 }
 
+void PatchTrack::setPlaybackRate (const AudioPlaybackRate &playbackRate) {
+    if (mProxy != nullptr) {
+        sp<AudioTrackClientProxy>::cast(mProxy)->setPlaybackRate(playbackRate);
+    }
+}
+
 void PatchTrack::restartIfDisabled()
 {
     if (android_atomic_and(~CBLK_DISABLED, &mCblk->mFlags) & CBLK_DISABLED) {
