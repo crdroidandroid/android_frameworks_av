f5101dadeba06669ede9a668ca6c11862500ffaa
kuowei.li <Kuowei.li@mediatek.com>
2025-12-15 14:34:46 +0800

    audio: fix incorrect eos in sync offload output case.
    Mixer status set incorrectly when async write is not used, causing
    test failure.
    Adjust mixer status to MIXER_DRAIN_ALL when async write is
    disabled.
    
    Test: atest CtsMediaPlayerTestCases:android.media.player.cts.MediaPlayerTest#testPositionAtEnd
    Change-Id: I4b4338a1f57e79750c9c02e90391ea7089a180a6

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -7628,7 +7628,11 @@ PlaybackThread::mixer_state OffloadThread::prepareTracks_l(
                             if ((mDrainSequence & 1) == 0) {
                                 mSleepTimeUs = 0;
                                 mStandbyTimeNs = systemTime() + mStandbyDelayNs;
-                                mixerStatus = MIXER_DRAIN_TRACK;
+                                if (!mUseAsyncWrite) {
+                                    mixerStatus = MIXER_DRAIN_ALL;
+                                } else {
+                                    mixerStatus = MIXER_DRAIN_TRACK;
+                                }
                                 mDrainSequence += 2;
                             }
                             if (mHwPaused) {
